ff9962cb8f9e0ad2834391441ab487caba9e4db3
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-02-27 17:17:15 +0600

    updated

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 
 import logging
 import os
-import sys
 import time
 
 from PySide2.QtCore import Slot
 from PySide2.QtGui import QPixmap, QGuiApplication, QCloseEvent
-from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QStyleFactory, QMessageBox
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 
 from hashcalc import HashingMethods
 from ui.ui_mainwindow import Ui_MainWindow
@@ -161,46 +160,5 @@ class MainWindow(QMainWindow):
         self.ui.lineEditCheckHashBox.setText(self.__clipboard.text())
 
 
-def main():
-    app = QApplication(sys.argv)
-    app.setStyle(QStyleFactory.create('Fusion'))
-
-    # qApp.setStyle(QStyleFactory.create("Fusion"))
-    #
-    # darkPalette = QPalette()
-    #
-    # darkColor = QColor(45, 45, 45)
-    # disabledColor = QColor(127, 127, 127)
-    # darkPalette.setColor(QPalette.Window, darkColor)
-    # darkPalette.setColor(QPalette.WindowText, Qt.white)
-    # darkPalette.setColor(QPalette.Base, QColor(40, 40, 40))
-    # darkPalette.setColor(QPalette.AlternateBase, darkColor)
-    # darkPalette.setColor(QPalette.ToolTipBase, Qt.white)
-    # darkPalette.setColor(QPalette.ToolTipText, Qt.white)
-    # darkPalette.setColor(QPalette.Text, Qt.white)
-    # darkPalette.setColor(QPalette.Disabled, QPalette.Text, disabledColor)
-    # darkPalette.setColor(QPalette.Button, darkColor)
-    # darkPalette.setColor(QPalette.ButtonText, Qt.white)
-    # darkPalette.setColor(QPalette.Disabled, QPalette.ButtonText, disabledColor)
-    # darkPalette.setColor(QPalette.BrightText, Qt.red)
-    # darkPalette.setColor(QPalette.Link, QColor(42, 130, 218))
-    # darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-    # darkPalette.setColor(QPalette.HighlightedText, Qt.black)
-    # darkPalette.setColor(QPalette.Disabled, QPalette.HighlightedText, disabledColor)
-    #
-    # qApp.setPalette(darkPalette)
-    #
-    # qApp.setStyleSheet("""
-    #     QToolTip {
-    #         color: #ffffff;
-    #         background-color: #2A82DA;
-    #         border: 1px solid white;
-    #     }""")
-
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
-
-
 if __name__ == "__main__":
-    main()
+    print('Hello World')
