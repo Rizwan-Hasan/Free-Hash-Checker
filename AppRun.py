9a4cc76f321c9b0eb0934ec4f3bf7d84fa340e0c
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-03-01 18:56:29 +0600

    updated

--- AppRun.py
+++ AppRun.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+"""
+__author__ = "Rizwan Hasan"
+__copyright__ = "Copyright 2020"
+__license__ = "MIT"
+__version__ = "3.0"
+__maintainer__ = "Rizwan Hasan"
+__email__ = "rizwan.hasan486@gmail.com"
+
+"""
+
 import sys
 
 from PySide2.QtGui import qApp
