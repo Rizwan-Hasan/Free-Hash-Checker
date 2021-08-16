df82820b73ab24e559e65382db068de9b29a2aad
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-08-16 22:36:45 +0600

    updated

--- ui/ui_mainwindow.py
+++ ui/ui_mainwindow.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainwindow.ui'
+## Form generated from reading UI file 'mainwindowxRKzTr.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.14.2
+## Created by: Qt User Interface Compiler version 5.15.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
-                            QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-                           QFontDatabase, QIcon, QKeySequence, QLinearGradient,
-                           QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtCore import *
+from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 import resources_rc
@@ -24,12 +21,11 @@ class Ui_MainWindow(object):
             MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowModality(Qt.NonModal)
         MainWindow.setEnabled(True)
-        MainWindow.resize(511, 321)
+        MainWindow.resize(511, 372)
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            MainWindow.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMinimumSize(QSize(0, 0))
         icon = QIcon()
@@ -45,8 +41,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.tabWidget = QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName("tabWidget")
-        sizePolicy.setHeightForWidth(
-            self.tabWidget.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
         self.tabWidget.setSizePolicy(sizePolicy)
         self.tabWidget.setTabPosition(QTabWidget.North)
         self.tabWidget.setTabShape(QTabWidget.Rounded)
@@ -83,8 +78,7 @@ class Ui_MainWindow(object):
         self.buttonSelectFile = QPushButton(self.groupBoxHashCalculation)
         self.buttonSelectFile.setObjectName("buttonSelectFile")
         icon1 = QIcon()
-        icon1.addFile(":/select/import-file.png", QSize(), QIcon.Normal,
-                      QIcon.Off)
+        icon1.addFile(":/select/import-file.png", QSize(), QIcon.Normal, QIcon.Off)
         self.buttonSelectFile.setIcon(icon1)
         self.buttonSelectFile.setIconSize(QSize(20, 21))
 
@@ -126,8 +120,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_2.setContentsMargins(0, -1, -1, -1)
         self.comboBoxHashChoices = QComboBox(self.groupBoxHashCalculation)
         icon4 = QIcon()
-        icon4.addFile(":/rhombus/rhombus.png", QSize(), QIcon.Normal,
-                      QIcon.Off)
+        icon4.addFile(":/rhombus/rhombus.png", QSize(), QIcon.Normal, QIcon.Off)
         self.comboBoxHashChoices.addItem(icon4, "")
         self.comboBoxHashChoices.addItem(icon4, "")
         self.comboBoxHashChoices.addItem(icon4, "")
@@ -138,30 +131,29 @@ class Ui_MainWindow(object):
         self.comboBoxHashChoices.setEnabled(True)
         self.comboBoxHashChoices.setEditable(False)
         self.comboBoxHashChoices.setSizeAdjustPolicy(
-            QComboBox.AdjustToContentsOnFirstShow)
+            QComboBox.AdjustToContentsOnFirstShow
+        )
         self.comboBoxHashChoices.setIconSize(QSize(16, 16))
         self.comboBoxHashChoices.setFrame(True)
 
         self.horizontalLayout_2.addWidget(self.comboBoxHashChoices)
 
-        self.progressBarHashCaclulation = QProgressBar(
-            self.groupBoxHashCalculation)
-        self.progressBarHashCaclulation.setObjectName(
-            "progressBarHashCaclulation")
+        self.progressBarHashCaclulation = QProgressBar(self.groupBoxHashCalculation)
+        self.progressBarHashCaclulation.setObjectName("progressBarHashCaclulation")
         self.progressBarHashCaclulation.setMaximum(100)
         self.progressBarHashCaclulation.setValue(0)
         self.progressBarHashCaclulation.setTextVisible(True)
         self.progressBarHashCaclulation.setInvertedAppearance(False)
-        self.progressBarHashCaclulation.setTextDirection(
-            QProgressBar.TopToBottom)
+        self.progressBarHashCaclulation.setTextDirection(QProgressBar.TopToBottom)
 
         self.horizontalLayout_2.addWidget(self.progressBarHashCaclulation)
 
         self.buttonHashCalculate = QPushButton(self.groupBoxHashCalculation)
         self.buttonHashCalculate.setObjectName("buttonHashCalculate")
         icon5 = QIcon()
-        icon5.addFile(":/calculate/drawing-compass.png", QSize(), QIcon.Normal,
-                      QIcon.Off)
+        icon5.addFile(
+            ":/calculate/drawing-compass.png", QSize(), QIcon.Normal, QIcon.Off
+        )
         self.buttonHashCalculate.setIcon(icon5)
         self.buttonHashCalculate.setIconSize(QSize(20, 21))
         self.buttonHashCalculate.setCheckable(False)
@@ -198,8 +190,7 @@ class Ui_MainWindow(object):
         self.buttonCheckHash = QPushButton(self.groupBoxHashMatcher)
         self.buttonCheckHash.setObjectName("buttonCheckHash")
         icon6 = QIcon()
-        icon6.addFile(":/check/magnifying-glass.png", QSize(), QIcon.Normal,
-                      QIcon.Off)
+        icon6.addFile(":/check/magnifying-glass.png", QSize(), QIcon.Normal, QIcon.Off)
         self.buttonCheckHash.setIcon(icon6)
         self.buttonCheckHash.setIconSize(QSize(20, 21))
 
@@ -318,12 +309,11 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName("menubar")
-        self.menubar.setGeometry(QRect(0, 0, 511, 21))
+        self.menubar.setGeometry(QRect(0, 0, 511, 27))
         MainWindow.setMenuBar(self.menubar)
 
         self.retranslateUi(MainWindow)
-        self.buttonClearCheckHashBox.clicked.connect(
-            self.lineEditCheckHashBox.clear)
+        self.buttonClearCheckHashBox.clicked.connect(self.lineEditCheckHashBox.clear)
 
         self.tabWidget.setCurrentIndex(0)
         self.buttonHashCalculate.setDefault(False)
@@ -334,112 +324,156 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(
-            QCoreApplication.translate("MainWindow", "Free Hash Checker",
-                                       None))
+            QCoreApplication.translate("MainWindow", u"Free Hash Checker", None)
+        )
         self.groupBoxHashCalculation.setTitle(
-            QCoreApplication.translate("MainWindow", "Hash Calculation", None))
+            QCoreApplication.translate("MainWindow", u"Hash Calculation", None)
+        )
         self.labelFileExplore.setText("")
         self.lineEditFileExplore.setPlaceholderText(
-            QCoreApplication.translate("MainWindow", "Select a file to begin",
-                                       None))
+            QCoreApplication.translate("MainWindow", u"Select a file to begin", None)
+        )
         self.buttonSelectFile.setText(
-            QCoreApplication.translate("MainWindow", "Select file", None))
+            QCoreApplication.translate("MainWindow", u"Select file", None)
+        )
         self.buttonClearHashBox.setText(
-            QCoreApplication.translate("MainWindow", "Clear", None))
+            QCoreApplication.translate("MainWindow", u"Clear", None)
+        )
         self.lineEditHashBox.setText("")
         self.lineEditHashBox.setPlaceholderText(
-            QCoreApplication.translate("MainWindow",
-                                       "Hash will be displayed here", None))
+            QCoreApplication.translate(
+                "MainWindow", u"Hash will be displayed here", None
+            )
+        )
         self.buttonCopyToClipboard.setText(
-            QCoreApplication.translate("MainWindow", "Copy", None))
+            QCoreApplication.translate("MainWindow", u"Copy", None)
+        )
         self.comboBoxHashChoices.setItemText(
-            0, QCoreApplication.translate("MainWindow", "MD5", None))
+            0, QCoreApplication.translate("MainWindow", u"MD5", None)
+        )
         self.comboBoxHashChoices.setItemText(
-            1, QCoreApplication.translate("MainWindow", "SHA1", None))
+            1, QCoreApplication.translate("MainWindow", u"SHA1", None)
+        )
         self.comboBoxHashChoices.setItemText(
-            2, QCoreApplication.translate("MainWindow", "SHA224", None))
+            2, QCoreApplication.translate("MainWindow", u"SHA224", None)
+        )
         self.comboBoxHashChoices.setItemText(
-            3, QCoreApplication.translate("MainWindow", "SHA256", None))
+            3, QCoreApplication.translate("MainWindow", u"SHA256", None)
+        )
         self.comboBoxHashChoices.setItemText(
-            4, QCoreApplication.translate("MainWindow", "SHA384", None))
+            4, QCoreApplication.translate("MainWindow", u"SHA384", None)
+        )
         self.comboBoxHashChoices.setItemText(
-            5, QCoreApplication.translate("MainWindow", "SHA512", None))
+            5, QCoreApplication.translate("MainWindow", u"SHA512", None)
+        )
 
         self.comboBoxHashChoices.setCurrentText(
-            QCoreApplication.translate("MainWindow", "MD5", None))
+            QCoreApplication.translate("MainWindow", u"MD5", None)
+        )
         self.progressBarHashCaclulation.setFormat(
-            QCoreApplication.translate("MainWindow", "%p%", None))
+            QCoreApplication.translate("MainWindow", u"%p%", None)
+        )
         self.buttonHashCalculate.setText(
-            QCoreApplication.translate("MainWindow", "Calculate", None))
+            QCoreApplication.translate("MainWindow", u"Calculate", None)
+        )
         self.groupBoxHashMatcher.setTitle(
-            QCoreApplication.translate("MainWindow", "Hash Matcher", None))
+            QCoreApplication.translate("MainWindow", u"Hash Matcher", None)
+        )
         self.buttonClearCheckHashBox.setText(
-            QCoreApplication.translate("MainWindow", "Clear", None))
+            QCoreApplication.translate("MainWindow", u"Clear", None)
+        )
         self.lineEditCheckHashBox.setPlaceholderText(
-            QCoreApplication.translate("MainWindow", "Paste hash here", None))
+            QCoreApplication.translate("MainWindow", u"Paste hash here", None)
+        )
         self.buttonCheckHash.setText(
-            QCoreApplication.translate("MainWindow", "Check", None))
+            QCoreApplication.translate("MainWindow", u"Check", None)
+        )
         self.tabWidget.setTabText(
             self.tabWidget.indexOf(self.MainTab),
-            QCoreApplication.translate("MainWindow", "Home", None))
+            QCoreApplication.translate("MainWindow", u"Home", None),
+        )
         self.developerName.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p><span\n"
-                "                                                                style=\" font-size:10pt; font-weight:600;\">Developer:</span>\n"
+                "MainWindow",
+                u"<html><head/><body><p><span\n"
+                '                                                                style=" font-size:10pt; font-weight:600;">Developer:</span>\n'
                 "                                                                Rizwan Hasan</span></p></body></html>\n"
                 "                                                            ",
-                None))
+                None,
+            )
+        )
         self.developerEmail.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p><span\n"
-                "                                                                style=\" font-size:10pt; font-weight:600;\">Email:\n"
+                "MainWindow",
+                u"<html><head/><body><p><span\n"
+                '                                                                style=" font-size:10pt; font-weight:600;">Email:\n'
                 "                                                                </span>rizwan.hasan486@gmail.com </p></body></html>\n"
                 "                                                            ",
-                None))
+                None,
+            )
+        )
         self.logoCreditName.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p><span\n"
-                "                                                                style=\" font-size:10pt; font-weight:600;\">Logo\n"
+                "MainWindow",
+                u"<html><head/><body><p><span\n"
+                '                                                                style=" font-size:10pt; font-weight:600;">Logo\n'
                 "                                                                credit: </span>Sakib Khan Inan </p></body></html>\n"
                 "                                                            ",
-                None))
+                None,
+            )
+        )
         self.logoCreditEmail.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p><span\n"
-                "                                                                style=\" font-size:10pt; font-weight:600;\">Email:\n"
+                "MainWindow",
+                u"<html><head/><body><p><span\n"
+                '                                                                style=" font-size:10pt; font-weight:600;">Email:\n'
                 "                                                                </span>sakib.khaninan@hotmail.com </p></body></html>\n"
                 "                                                            ",
-                None))
+                None,
+            )
+        )
         self.icons8Credit.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p><span\n"
-                "                                                                style=\" font-size:10pt; font-weight:600;\">Icons\n"
+                "MainWindow",
+                u"<html><head/><body><p><span\n"
+                '                                                                style=" font-size:10pt; font-weight:600;">Icons\n'
                 "                                                                credit: </span>Icons8</p></body></html>\n"
                 "                                                            ",
-                None))
+                None,
+            )
+        )
         self.hashCheckerHeader.setText("")
         self.groupBox_2.setTitle(
-            QCoreApplication.translate("MainWindow", "GitHub", None))
+            QCoreApplication.translate("MainWindow", u"GitHub", None)
+        )
         self.sourceCodeLink.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p\n"
-                "                                                                                    align=\"center\"><a\n"
-                "                                                                                    href=\"https://github.com/Rizwan-Hasan/Free-Hash-Checker\"><img\n"
-                "                                                                                    src=\":/github/github.png\"/></a></p></body></html>\n"
+                "MainWindow",
+                u"<html><head/><body><p\n"
+                '                                                                                    align="center"><a\n'
+                '                                                                                    href="https://github.com/Rizwan-Hasan/Free-Hash-Checker"><img\n'
+                '                                                                                    src=":/github/github.png"/></a></p></body></html>\n'
                 "                                                                                ",
-                None))
+                None,
+            )
+        )
         self.groupBox_4.setTitle(
-            QCoreApplication.translate("MainWindow", "Version", None))
+            QCoreApplication.translate("MainWindow", u"Version", None)
+        )
         self.applicationVersion.setText(
             QCoreApplication.translate(
-                "MainWindow", "<html><head/><body><p\n"
-                "                                                                                    align=\"center\"><span\n"
-                "                                                                                    style=\" font-size:22pt;\n"
-                "                                                                                    font-weight:600;\">0.0</span></p></body></html>\n"
+                "MainWindow",
+                u"<html><head/><body><p\n"
+                '                                                                                    align="center"><span\n'
+                '                                                                                    style=" font-size:22pt;\n'
+                '                                                                                    font-weight:600;">0.0</span></p></body></html>\n'
                 "                                                                                ",
-                None))
+                None,
+            )
+        )
         self.tabWidget.setTabText(
             self.tabWidget.indexOf(self.AboutTab),
-            QCoreApplication.translate("MainWindow", "About", None))
+            QCoreApplication.translate("MainWindow", u"About", None),
+        )
 
     # retranslateUi
