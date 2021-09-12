91437469fae940506a959c7c791cea7038a6e736
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-09-13 01:08:24 +0600

    updated

--- winBuild.cmd
+++ winBuild.cmd
@@ -2,24 +2,24 @@
 
 rmdir /s /q "build"
 rmdir /s /q "dist"
-del /s /q "Free-Hash-Checker.spec"
+del /s /q "Free Hash Checker.spec"
 
 pyinstaller -y ^
---console ^
---name "Free-Hash-Checker" ^
+--windowed ^
+--onedir ^
+--clean ^
+--noupx ^
+--name "Free Hash Checker" ^
 --icon "./logo/icon.ico" ^
 --add-data "./ui";"./ui/" ^
 --add-data "./resources_rc.py";"." ^
 --add-data "LICENSE";"." ^
 --hidden-import "requests" ^
---version-file "version-file.txt" ^ "./AppRun.py" ^
---clean
+--version-file "version-file.txt" ^ "./AppRun.py"
 
 rmdir /s /q "build"
-del /s /q "Free-Hash-Checker.spec"
-rmdir /s /q "dist\\Free-Hash-Checker\\ui\\__pycache__"
-del /s /q "dist\\Free-Hash-Checker\\ui\\"*.png
-del /s /q "dist\\Free-Hash-Checker\\ui\\"*.qrc
-del /s /q "dist\\Free-Hash-Checker\\ui\\"*.ui
-del /s /q "dist\\Free-Hash-Checker\\ui\\__init__.py"
-pause
+del /s /q "Free Hash Checker.spec"
+rmdir /s /q "dist\\Free Hash Checker\\ui\\__pycache__"
+del /s /q "dist\\Free Hash Checker\\ui\\"*.png
+del /s /q "dist\\Free Hash Checker\\ui\\"*.qrc
+del /s /q "dist\\Free Hash Checker\\ui\\"*.ui
